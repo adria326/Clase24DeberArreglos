d957f054d0fad90941b540bbf50663e7fae5ac4e
Adriana <adriana.murillo@tecnologicoliceocristiano.edu.ec>
2024-08-12 16:23:53 -0500

    Deber #4

---
+++ Deber #4 Clase24DeberFunciones/Deber-02.py
@@ -0,0 +1,7 @@
+def saludo_personalizado(nombre):
+    print(f"¡Hola, {nombre}! ¿como estas?.")
+
+saludo_personalizado("Lili")
+saludo_personalizado("Darling")
+saludo_personalizado("Ronald")
+saludo_personalizado("Madeleine")
